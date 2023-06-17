d3647daa0179c90ecf185855e14fb0f0a81624c0
Nick Morgan <hello@morganick.com>
2023-06-17 09:38:18 -0400

    :sparkles: Adding testing library and passing test

---
+++ tests/App.test.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { render } from "@testing-library/react-native"
+
+import App from "../App"
+
+describe("<App />", () => {
+  it("has 1 child", () => {
+    const tree = render(<App />).toJSON()
+    expect(tree.children.length).toBe(1)
+  })
+})
